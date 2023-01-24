cf23fc933ca08ad240784ab9e190bbcfcdd4fbd1
Carlos Minero Dubon <caminero@excelautomotriz.com>
2023-01-23 18:53:05 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Carlos Dubon on 23/1/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
